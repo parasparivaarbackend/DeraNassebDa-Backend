eaece5b415e89dea9eeeaa851dd98237a1675ac2
topDevelopers123 <parasjisco@gmail.com>
2024-11-30 16:46:33 +0530

    fix image

--- src/middleware/multter.middleware.js
+++ src/middleware/multter.middleware.js
@@ -1,34 +1,31 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-        // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-})
-
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, `Dera_${file.originalname}-${Date.now()} `);
+  },
+});
 
 const fileFilter = (req, file, cb) => {
-    const fileTypes = /jpeg|jpg|png|webp/;
-    const extname = fileTypes.test(file.originalname.toLowerCase());
-    const mimetype = fileTypes.test(file.mimetype)
-    if (extname && mimetype) {
-        return cb(null, true)
-    } else {
-        cb(new Error(`only images are allowed ${fileTypes}`))
-    }
-}
-
+  const fileTypes = /jpeg|jpg|png|webp/;
+  const extname = fileTypes.test(file.originalname.toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(new Error(`only images are allowed ${fileTypes}`));
+  }
+};
 
 export const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: {
-        files: 3, // Limit to 3 files
-        fileSize: 500 * 1024
-    }
-})
-
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    files: 3, // Limit to 3 files
+    fileSize: 500 * 1024,
+  },
+});
